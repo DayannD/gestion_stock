bdbad32b13292dbc868b9b2e6a11017267e70f1d
dayann <dayanndeshayes@gmail.com>
2022-08-25 16:39:00 +0200

    Ajout d'exception personnalisÃé

---
+++ src/main/java/com/deshayes/gestiondestock/exception/EntityNotFoundException.java
@@ -0,0 +1,27 @@
+package com.deshayes.gestiondestock.exception;
+
+import lombok.Getter;
+
+public class EntityNotFoundException extends RuntimeException{
+
+    @Getter
+    private ErrorCodes errorCodes;
+
+    public EntityNotFoundException(String message){
+        super(message);
+    }
+
+    public EntityNotFoundException(String message,Throwable cause){
+        super(message,cause);
+    }
+
+    public EntityNotFoundException(String message,Throwable cause,ErrorCodes errorCodes){
+        super(message,cause);
+        this.errorCodes = errorCodes;
+    }
+
+    public EntityNotFoundException(String message,ErrorCodes errorCodes){
+        super(message);
+        this.errorCodes = errorCodes;
+    }
+}
