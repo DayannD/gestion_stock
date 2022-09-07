b41f88c669b3f4710074b0e7c7e98a6521b0405a
dayann <dayanndeshayes@gmail.com>
2022-09-07 19:11:08 +0200

    ajout de mon service et interface fournisseur et modification de quelque class

--- src/main/java/com/deshayes/gestiondestock/repository/CommandeClientRepository.java
+++ src/main/java/com/deshayes/gestiondestock/repository/CommandeClientRepository.java
@@ -3,5 +3,8 @@ package com.deshayes.gestiondestock.repository;
 import com.deshayes.gestiondestock.model.CommandeClient;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface CommandeClientRepository extends JpaRepository<CommandeClient, Integer> {
+    Optional<CommandeClient> findCommandeClientByCode(String code);
 }
